5221899901cf77a21eac7f27234b73ab33417fc6
noctismal <190322123+Noctismal@users.noreply.github.com>
2025-05-26 19:51:09 +0100

    getting 1 files signiture

--- main.c
+++ main.c
@@ -2,8 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Globals */ 
+/* Macros */ 
 #define MIN_ARGC 2
+
+struct file_info {
+    char *f_name;
+    unsigned char *sig;
+};
+
+/* Prototypes */ 
+void *allocate(size_t size);
+struct file_info *get_file_info(char *file);
+unsigned char *get_sig(char *file);
 
 int main(int argc, char *argv[]) {
     // Make sure minimum amount of arguments were given
@@ -12,8 +22,51 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    for (int i =0; i < argc; i++) {
-         printf("%s\n", argv[i]);
-    }
+    // Working with only one file atm TODO 
+    struct file_info *f1 = get_file_info(argv[1]);
+
+    printf("%s\n", f1->f_name);
+
+    for (int i = 0; i < 7; i++) {
+        printf("%02X ", f1->sig[i]);
+    };
+
     return 0;
 }
+
+void *allocate(size_t size) {
+    void *ptr = malloc(size);
+
+    if (ptr == NULL) {
+        fprintf(stderr, "Error malloc failled\n");
+        exit(1);
+    };
+
+    return ptr;
+}
+
+struct file_info *get_file_info(char *file) {
+    struct file_info *f1 = allocate(sizeof(struct file_info));
+
+    f1->f_name = file;
+
+    f1->sig = get_sig(file);
+    
+    return f1;
+}
+
+unsigned char *get_sig(char *file) {
+    FILE *f = fopen(file, "rb");
+
+    unsigned char *buff = allocate(sizeof(unsigned char) * 8);
+
+    if (f == NULL) {
+        fprintf(stderr, "Error opening file: %s\n", file);
+        exit(1);
+    };
+
+    fread(buff, 8, 1, f);
+
+    return buff;
+}
+
