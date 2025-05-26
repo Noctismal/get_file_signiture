25b507ded683bd498f3d1d9039cfed84ddc15826
noctismal <190322123+Noctismal@users.noreply.github.com>
2025-05-26 18:49:40 +0100

    added minimum argc check

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
+/* Dependencies */
 #include <stdio.h>
+#include <stdlib.h>
+
+/* Globals */ 
+#define MIN_ARGC 2
 
 int main(int argc, char *argv[]) {
+    // Make sure minimum amount of arguments were given
+    if (argc < MIN_ARGC) {
+        fprintf(stderr, "Error: Not enough arguments.\n");
+        exit(1);
+    }
+
     for (int i =0; i < argc; i++) {
          printf("%s\n", argv[i]);
     }
